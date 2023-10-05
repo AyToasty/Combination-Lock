689c04cbdc9496baf5c1ddca2547e4f8647206b2
AyToasty <danhcao65@gmail.com>
2023-10-05 12:47:11 -0500

    Final Product

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-// Create initial string
-const message = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
-console.log(message);
+// Create initial string message
+const message = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
 
 // Assign 3 variables
 let firstcode, secondcode, thirdcode;
 
-/* First code using + */
-firstcode = 3 + 7;
-console.log('The first code will be:', firstcode);
+/* First code equalling 10 */
+firstcode = 1 + 5 * 3 - 6
+console.log('The first code will be:', firstcode)
 
-/* Second code using */
-secondcode = 8 * 5;
-console.log('The second code will be:', secondcode);
+/* Second code equalling 40*/
+secondcode = 27 / 3 * 4 + 4
+console.log('The second code will be:', secondcode)
 
-/* Third code using - */
-thirdcode = 100 - 61;
-console.log('The third code will be:', thirdcode);
+/* Third code equalling 39 */
+thirdcode = 100 * 5 / 25 +20 - 1
+console.log('The third code will be:', thirdcode)
 
-alert(message + ' ' + firstcode + ', ' + secondcode + ', ' + thirdcode);
+// Create alert popup for message
+alert(message + ' ' + firstcode + ', ' + secondcode + ', ' + thirdcode)
