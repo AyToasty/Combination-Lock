fa1ce23729b481b22ef98317779e38617ce8ae02
AyToasty <danhcao65@gmail.com>
2023-10-05 12:24:00 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Create initial string
+const message = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
+console.log(message);
+
+// Assign 3 variables
+let firstcode, secondcode, thirdcode;
+
+/* First code using + */
+firstcode = 3 + 7;
+console.log('The first code will be:', firstcode);
+
+/* Second code using */
+secondcode = 8 * 5;
+console.log('The second code will be:', secondcode);
+
+/* Third code using - */
+thirdcode = 100 - 61;
+console.log('The third code will be:', thirdcode);
+
+alert(message + ' ' + firstcode + ', ' + secondcode + ', ' + thirdcode);
